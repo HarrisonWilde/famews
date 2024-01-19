cae138d3457c3093a71de10a2233cfe260b9f875
Manuel Burger <manuel.burger@inf.ethz.ch>
2024-01-19 12:44:09 +0100

    Full author list on package

--- setup.py
+++ setup.py
@@ -21,12 +21,15 @@ def get_version(rel_path):
 setuptools.setup(
     name="famews",
     version=get_version("famews/__init__.py"),
-    author="Marine Hoche, Olga Mineeva, Manuel Burger",
+    author="Marine Hoche, Olga Mineeva, Manuel Burger, Alessandro Blasimme, Gunnar Rätsch",
     url="https://github.com/ratschlab/famews",
     license="MIT License",
     description="FAMEWS: A Fairness Auditing tool for Medical Early-Warning Systems",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    keywords = [
+        'Fairness', 'Machine Learning',
+        'Early-Warning System', 'Clinical Applications'],   # Keywords that define your package best
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
